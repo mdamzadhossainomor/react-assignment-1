0fa65841bbc66e266e7af07afc8904dd75d66b85
Md Amzad Hossain Omor <139708044+mdamzadhossainomor@users.noreply.github.com>
2024-12-02 15:29:21 +0600

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Hero from './component/Hero'
+import Nav from './component/Nav'
+function App() {
+
+
+  return (
+    <>
+      <Nav />
+      
+      <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <>
+            <div className='h-[572px] flex justify-end w-[100%]'>
+                <div>
+                    <img src="/Group1.svg" alt="" className='w-full' />
+              </div>
+                
+            </div>
+        </>
+    )
+}
+
+export default Hero
