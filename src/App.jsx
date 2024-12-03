dac60d6771f62148c593ef02d5230de2bacd7530
Md Amzad Hossain Omor <139708044+mdamzadhossainomor@users.noreply.github.com>
2024-12-03 22:38:35 +0600

    partnersData add

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 
 import './App.css'
+import Carousel from './component/Carousel'
+import Footer from './component/Footer'
+import GetStart from './component/GetStart'
+import Handle from './component/Handle'
 import Hero from './component/Hero'
+import Info from './component/Info'
 import Nav from './component/Nav'
+import Partners from './component/Partners'
+import Preferedemail from './component/Preferedemail'
 function App() {
-
-
+  
   return (
     <>
       <Nav />
-      
-      <Hero/>
+      <Hero />
+      <Partners />
+      <Info />
+      <Preferedemail/>
+      <Carousel />
+      <Handle />
+      <GetStart />
+      <Footer/>
     </>
   )
 }
---
+++ src/component/Partners.jsx
@@ -0,0 +1,38 @@
+import { div } from 'framer-motion/client'
+import React from 'react'
+
+function Partners() {
+    const partnersData = [
+        {
+            img:"BackHublogo.svg"
+        },
+        {
+            img:"CableLabslogo.svg"
+        },
+        {
+            img:"DBSlogo.svg"
+        },
+        {
+            img:"EasyEurologo.svg"
+        },
+        {
+            img:"AMDlogo.svg"
+        }
+
+    ]
+
+  return (
+      <>
+          <section className='w-[100%] bg-[#07292F]'>
+            
+                  <h3 className='
+                  text-center text-[#0FF1F6] text-sm pt-[32px]'>OUR trusted partners</h3>
+            
+
+
+          </section>
+      </>
+  )
+}
+
+export default Partners
