1788ab70854bceed0c9876afacaa7e2d825631d8
Md Amzad Hossain Omor <139708044+mdamzadhossainomor@users.noreply.github.com>
2024-12-03 23:21:22 +0600

    some text size change

--- src/component/GetStart.jsx
+++ src/component/GetStart.jsx
@@ -2,24 +2,25 @@ import React from 'react'
 import Button from './ComButton'
 function GetStart() {
   return (
-      <>
-          <div className='bg-[#002228] text-white pt-[96px]  h-[525px] md:h-[433px] lg:h-[443px]'>
-              <h3 className='text-center  text-[#0FF1F6]'>
-                  GET STARTED
-              </h3>
+    <>
+      <div className="bg-[#002228] text-white pt-[96px]  h-[525px] md:h-[433px] lg:h-[443px]">
+        <h3 className="text-center  text-[#0FF1F6]">GET STARTED</h3>
 
-              <h2 className='text-center text-[36px] md:w-[640px] mx-auto mt-[23px]'>
-                  Embrace the new era of outbound.
-              </h2>
-              <p className='text-center mt-[23px]'>Wizia lets you train, activate, and optimize aiDRs.
-                  Take your outbound to new levels of performance and efficiency.</p>
-              
-              <div className='flex justify-center mt-[23px]'>
-                  <Button />
-             </div>
+        <h2 className="text-center text-[36px] md:w-[640px] mx-auto mt-[23px] font-bold">
+          <span className="italic font-normal">Embrace</span> the new era of 
+           <span className="italic font-normal ps-2">outbound.</span>
+        </h2>
+        <p className="text-center mt-[23px]">
+          Wizia lets you train, activate, and optimize aiDRs. Take your outbound
+          to new levels of performance and efficiency.
+        </p>
+
+        <div className="flex justify-center mt-[23px]">
+          <Button />
+        </div>
       </div>
-      </>
-  )
+    </>
+  );
 }
 
 export default GetStart
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -3,28 +3,27 @@ import React from 'react'
 
 function Hero() {
     return (
-        <>
-           
-            <div className='bg-[url(/hero.svg)] w-full h-[408px] sm:h-[408px] md:h-[572px] lg:h-[572px] bg-cover bg-no-repeat bg-right-bottom flex '>
-                <div className='text-center sm:text-start md:text-start lg:text-start mt-[44px] sm:mt-[110px] sm:ml-[50px] md:ml-[120px] max-w-[1440px] '>
-                        <p className='text-[#0FF1F6]'>
-                            AI SDRs (aiDRs)
-                        </p>
+      <>
+        <div className="bg-[url(/hero.svg)] w-full h-[408px] sm:h-[408px] md:h-[572px] lg:h-[572px] bg-cover bg-no-repeat bg-right-bottom flex ">
+          <div className="text-center sm:text-start md:text-start lg:text-start mt-[44px] sm:mt-[110px] sm:ml-[50px] md:ml-[120px] max-w-[1440px] ">
+            <p className="text-[#0FF1F6]">AI SDRs (aiDRs)</p>
 
-                    <h3 className='text-5xl sm:text-[64px] md:text-[64px] lg:text-[64px] font-bold text-white sm:w-[380px] md:w-[391px] lg:w-[391px] mt-[4px]'>
-                            More leads, less people.
-                        </h3>
+            <h3 className="text-5xl sm:text-[64px] md:text-[64px] lg:text-[64px] font-bold text-white sm:w-[380px] md:w-[391px] lg:w-[391px] mt-[4px]">
+              More <span className="italic font-normal">leads,</span> less{" "}
+              <span className="italic font-normal">people.</span>
+            </h3>
 
-                    <p className='text-[#FFFFFF] sm:w-[343px] md:w-[450px] mt-[10px]'>
-                            Train an aiDR on your ICP and messaging matrix. Activate it on a patch. It will send personalized sequences to every target contact.
-                        </p>
+            <p className="text-[#FFFFFF] sm:w-[343px] md:w-[450px] mt-[10px]">
+              Train an aiDR on your ICP and messaging matrix. Activate it on a
+              patch. It will send personalized sequences to every target
+              contact.
+            </p>
 
-                    <Button/>
-                    </div>
-                </div>
-          
-        </>
-    )
+            <Button />
+          </div>
+        </div>
+      </>
+    );
 }
 
 export default Hero
