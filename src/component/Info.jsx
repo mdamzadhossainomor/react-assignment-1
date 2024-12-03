f6423d9d4503499d8fbd1702724047242c82a0a8
Md Amzad Hossain Omor <139708044+mdamzadhossainomor@users.noreply.github.com>
2024-12-03 22:41:01 +0600

    info data add

---
+++ src/component/Info.jsx
@@ -0,0 +1,35 @@
+import { img } from "framer-motion/client";
+import React from "react";
+
+function Info() {
+  const infoData = [
+    {
+      img: "info1.svg",
+      title: "You’re in Control",
+      description:
+        "aiDRs operate based on your rules. You guide the machine, the aiDRs execute the work.",
+    },
+    {
+      img: "info2.svg",
+      title: "Infinitely Scalable",
+      description:
+        "Train an aiDR on a patch. When you are happy with the results, scale it effortlessly.",
+    },
+    {
+      img: "info3.svg",
+      title: "Incredibly Flexible",
+      description:
+        "Train an aiDR on an outbound patch, to nurture inbound leads, or to reach out to your long tail of SMB customers at renewal time.",
+    },
+  ];
+
+  return (
+    <>
+      <section className=" bg-[#002228] text-white">
+        
+      </section>
+    </>
+  );
+}
+
+export default Info;
