37cc746a5c16d502b415200d8cbedfa4ae3d19c1
Md Amzad Hossain Omor <139708044+mdamzadhossainomor@users.noreply.github.com>
2024-12-03 22:42:43 +0600

    preferedmail body add

---
+++ src/component/Preferedemail.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Preferedemail() {
+  
+
+  return (
+    <>
+      <div className="bg-[#002228] flex justify-center text-white pt-6 pb-6 ">
+        <div className="bg-[url(BG.svg)] w-full pb-6 sm:w-[1344px] md:w-[1344px] lg:w-[1344px] h-auto md:h-[564px] bg-left-bottom bg-cover flex justify-center mx-5 rounded-2xl">
+          <div className=" pl-[24px] sm:pl-[72px] md:pl-[250px] lg:[486px] mt-[69px] ">
+            <h2 className="md:w-[591px lg:w-[591px] text-[32px]">
+              Train your aiDR on your...
+            </h2>
+            <h2 className="text-[#0FF1F6] text-[32px] italic">
+              prefered email st|
+            </h2>
+
+            <p
+              className="
+                      mt-[30px]"
+            >
+              You’re in control. Train your aiDR on elements of your Marketing
+              strategy.
+            </p>
+
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Preferedemail;
