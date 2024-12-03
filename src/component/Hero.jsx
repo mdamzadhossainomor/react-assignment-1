427b985faacc031a5fc48ce7f1d29c47cd57e344
Md Amzad Hossain Omor <139708044+mdamzadhossainomor@users.noreply.github.com>
2024-12-03 23:25:25 +0600

    hero px add

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     return (
       <>
         <div className="bg-[url(/hero.svg)] w-full h-[408px] sm:h-[408px] md:h-[572px] lg:h-[572px] bg-cover bg-no-repeat bg-right-bottom flex ">
-          <div className="text-center sm:text-start md:text-start lg:text-start mt-[44px] sm:mt-[110px] sm:ml-[50px] md:ml-[120px] max-w-[1440px] ">
+          <div className="text-center sm:text-start md:text-start lg:text-start mt-[44px] sm:mt-[110px] sm:ml-[50px] md:ml-[120px] max-w-[1440px] px-3">
             <p className="text-[#0FF1F6]">AI SDRs (aiDRs)</p>
 
             <h3 className="text-5xl sm:text-[64px] md:text-[64px] lg:text-[64px] font-bold text-white sm:w-[380px] md:w-[391px] lg:w-[391px] mt-[4px]">
